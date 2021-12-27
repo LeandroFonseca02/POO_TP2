846553bd026a4353e0bc66fce20f88a71a0b7074
Pedro Pinto <pedromcspinto@gmail.com>
2021-12-27 16:42:22 +0000

    FIX Client Class

--- src/Client.java
+++ src/Client.java
@@ -1,12 +1,6 @@
 import java.io.IOException;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        Extract objExtract = new Extract("Teste");
-        System.out.println(objExtract.getStrTeste());
-        Transform objTransform = new Transform("Teste2");
-        System.out.println(objTransform.getStrTeste());
-        Load objLoad = new Load("Teste5");
-        System.out.println(objLoad.getStrTeste());
+    public static void main(String[] args){
     }
 }
