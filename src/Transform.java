4c76e9ea55d9c3eb920dfa07cf3155a74ecefbd5
Pedro Pinto <pedromcspinto@gmail.com>
2021-12-27 18:00:16 +0000

    FIX Transform Class

--- src/Transform.java
+++ src/Transform.java
@@ -1,7 +1,21 @@
+import java.util.*;
+
 public class Transform {
-    Transform(String[][] strTeste){
-
+    private ArrayList<HashMap<String,String>> dataStorage;
+    Transform(String[][]strMatrixData){
+        dataStorage = new ArrayList<>();
+        transformData(strMatrixData, dataStorage);
     }
-
-
+    private void transformData(String[][] strMatrixData, ArrayList<HashMap<String,String>> dataStorage) {
+        for(int j = 0; j<strMatrixData[0].length; j++){
+            for(int i = 1; i<strMatrixData.length; i++){
+                HashMap<String, String> map = new HashMap<>();
+                map.put(strMatrixData[0][j], strMatrixData[i][j]);
+                dataStorage.add(map);
+            }
+        }
+    }
+    public ArrayList<HashMap<String, String>> getDataStorage() {
+        return dataStorage;
+    }
 }
