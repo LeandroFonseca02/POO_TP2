eecb8ae457fa95baf0f6fc3b393ea0b537a6187b
Pedro Pinto <pedromcspinto@gmail.com>
2021-12-27 16:39:50 +0000

    FIX Extract Class

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Extract objExtract = new Extract("Teste");
         System.out.println(objExtract.getStrTeste());
         Transform objTransform = new Transform("Teste2");
--- src/Extract.java
+++ src/Extract.java
@@ -1,10 +1,46 @@
+import java.io.*;
 public class Extract {
-    String strTeste;
-    Extract(String strTeste){
-        this.strTeste = strTeste;
-    }
 
-    public String getStrTeste() {
-        return strTeste;
+    private String[][] data;
+    private int intNumColumns, intNumLines;
+
+    Extract(String path) throws IOException {
+        countColumnsAndLines(path);
+        data=new String[getIntNumLines()][getIntNumColumns()];
+        readFile(data, path);
     }
+    public void readFile(String[][] data, String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        String line;
+        int i = 0;
+        while((line= br.readLine())!= null){
+            String[] SplittedString = line.split(",");
+            for(int j = 0; j<SplittedString.length; j++){
+                data[i][j]=SplittedString[j];
+            }
+            i++;
+        }
+        br.close();
+    }
+    public void countColumnsAndLines(String path) throws IOException {
+        BufferedReader counter = new BufferedReader(new FileReader(path));
+        int lines = 1;
+        String[] lineSplitted = counter.readLine().split(",");
+        String line;
+        setIntNumColumns(lineSplitted.length);
+        while((line = counter.readLine())!=null){lines++;}
+        setIntNumLines(lines);
+        counter.close();
+    }
+    public void setIntNumColumns(int value){this.intNumColumns = value;}
+    public void setIntNumLines(int value){
+        this.intNumLines = value;
+    }
+    public int getIntNumColumns(){
+        return this.intNumColumns;
+    }
+    public int getIntNumLines(){
+        return this.intNumLines;
+    }
+    public String[][] getOutput(){return data;}
 }
