71b9011e1f0bdf0290c0ee83db22e003b12f4ab4
Leandro Fonseca <leandro_dinis@hotmail.com>
2021-12-27 17:06:21 +0000

    FIX Client class
    FIX Extract class

--- src/Client.java
+++ src/Client.java
@@ -2,5 +2,12 @@ import java.io.IOException;
 
 public class Client {
     public static void main(String[] args){
+        try{
+            Extract objExtract = new Extract("./resources/Customer_Data.csv");
+            System.out.println("Numero Linhas : " + objExtract.getIntNumLines() + "\nNumero Colunas : " + objExtract.getIntNumColumns());
+        }catch (IOException ioException){
+            System.out.println("Ficheiro nao encontrado");
+        }
+
     }
 }
--- src/Extract.java
+++ src/Extract.java
@@ -1,46 +1,42 @@
 import java.io.*;
 public class Extract {
 
-    private String[][] data;
+    private final String[][] strMatrixData;
     private int intNumColumns, intNumLines;
 
-    Extract(String path) throws IOException {
-        countColumnsAndLines(path);
-        data=new String[getIntNumLines()][getIntNumColumns()];
-        readFile(data, path);
+    Extract(String strFilePath) throws IOException {
+        countColumnsAndLines(strFilePath);
+        strMatrixData = new String[this.getIntNumLines()][this.getIntNumColumns()];
+        readFile(strMatrixData, strFilePath);
     }
-    public void readFile(String[][] data, String path) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(path));
-        String line;
-        int i = 0;
-        while((line= br.readLine())!= null){
-            String[] SplittedString = line.split(",");
-            for(int j = 0; j<SplittedString.length; j++){
-                data[i][j]=SplittedString[j];
+    public void readFile(String[][] strMatrixData, String strFilePath) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(strFilePath));
+        String strLine;
+        for (int i = 0; (strLine = bufferedReader.readLine()) != null; i++){
+            String[] strSplittedLine = strLine.split(",");
+            for(int j = 0; j < strSplittedLine.length; j++){
+                strMatrixData[i][j] = strSplittedLine[j];
             }
-            i++;
         }
-        br.close();
+        bufferedReader.close();
     }
-    public void countColumnsAndLines(String path) throws IOException {
-        BufferedReader counter = new BufferedReader(new FileReader(path));
-        int lines = 1;
-        String[] lineSplitted = counter.readLine().split(",");
-        String line;
-        setIntNumColumns(lineSplitted.length);
-        while((line = counter.readLine())!=null){lines++;}
-        setIntNumLines(lines);
-        counter.close();
+    public void countColumnsAndLines(String strFilePath) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(strFilePath));
+        String[] strSplittedLine = bufferedReader.readLine().split(",");
+        int intNumLines;
+        String strLine;
+        setIntNumColumns(strSplittedLine.length);
+        for (intNumLines = 1; (strLine = bufferedReader.readLine()) !=null; intNumLines++);
+        setIntNumLines(intNumLines);
+        bufferedReader.close();
     }
-    public void setIntNumColumns(int value){this.intNumColumns = value;}
-    public void setIntNumLines(int value){
-        this.intNumLines = value;
-    }
-    public int getIntNumColumns(){
-        return this.intNumColumns;
-    }
-    public int getIntNumLines(){
-        return this.intNumLines;
-    }
-    public String[][] getOutput(){return data;}
+    public void setIntNumColumns(int intNumColumns){this.intNumColumns = intNumColumns;}
+
+    public void setIntNumLines(int intNumLines){this.intNumLines = intNumLines;}
+
+    public int getIntNumColumns(){return this.intNumColumns;}
+
+    public int getIntNumLines(){return this.intNumLines;}
+
+    public String[][] getStrMatrixData(){return this.strMatrixData;}
 }
