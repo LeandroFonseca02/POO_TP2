a1dc90f350f78a6a2671ba500865ca6a3dd62b54
Pedro Pinto <pedromcspinto@gmail.com>
2022-01-07 23:08:01 +0000

    FIX SUM
    FIX AVERAGE

--- src/Average.java
+++ src/Average.java
@@ -2,15 +2,35 @@ import java.util.*;
 
 public class Average extends Sum{
     private double average;
+
+    /**
+     * Contrutor da classe Average.
+     * Existe uma extração de uma coluna a partir dos dados do ficheiro
+     * para uma ArrayList de Strings, realizando uma média aos valores
+     * extraídos.
+     *
+     * @param data arraylist de hashmaps, onde os dados estão guardados.
+     * @param column chave da coluna a extraír os elementos.
+     */
     Average(ArrayList<HashMap<String, String>> data, String column) {
         super(data, column);
         calculateAverage(super.Output(), super.numberOfElements());
     }
 
+    /**
+     * Calcula a média de uma soma de elementos duma lista.
+     *
+     * @param sum soma de elementos de uma lista
+     * @param elems número de elementos de uma lista
+     */
     public void calculateAverage(double sum, int elems){
          average=sum/elems;
     }
 
+    /**
+     * Retorna a média dos elementos de uma lista
+     * @return média dos elementos de uma list
+     */
     public double Output(){
         return average;
     }
--- src/Sum.java
+++ src/Sum.java
@@ -4,11 +4,28 @@ public class Sum {
     private double sum;
     private ArrayList<String> extractedColumn;
 
+    /**
+     * Contrutor da classe Sum.
+     * Existe uma extração de uma coluna a partir dos dados do ficheiro
+     * para uma ArrayList de Strings, e soma todos os elementos da mesma
+     * guardado esse valor.
+     *
+     * @param data arraylist de hashmaps, onde os dados estão guardados.
+     * @param column chave da coluna a extraír os elementos.
+     */
     Sum(ArrayList<HashMap<String,String>> data, String column){
         extractedColumn = new ArrayList<>();
+        ExtractColumn(data, column);
         sumList();
     }
 
+    /**
+     * Extrai uma coluna de dados para uma ArrayList.
+     *
+     * @param data arraylist de hashmaps, onde os dados estão guardados.
+     * @param column chave da coluna a extraír os elementos.
+     * @return retorna a ArrayList com os elementos da coluna extraída.
+     */
     public ArrayList<String> ExtractColumn(ArrayList<HashMap<String,String>> data, String column){
         for (HashMap<String, String> map : data) {
             if (map.containsKey(column))
@@ -17,16 +34,29 @@ public class Sum {
         return extractedColumn;
     }
 
+    /**
+     *  Soma todos os elementos da ArrayList de Strings que contém os elementos
+     *  da coluna extraída.
+     *  Os valores são passados a double através de uma função parse.
+     */
     public void sumList(){
         for(String s: extractedColumn){
             sum += Double.parseDouble(s);
         }
     }
 
+    /**
+     * Retorna o número de elementos da coluna extraída.
+     * @return número de elementos da coluna
+     */
     public int numberOfElements(){
         return this.extractedColumn.size();
     }
 
+    /**
+     * Retorna a soma dos elementos da coluna extraída.
+     * @return Soma dos elementos da coluna extraída
+     */
     public double Output(){
         return this.sum;
     }
